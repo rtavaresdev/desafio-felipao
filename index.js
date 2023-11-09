17c5a749f799d0a7f1e09c5221ae2a856539bbd9
rtavaresdev <rtavares.adriano@gmail.com>
2023-11-09 18:14:45 -0300

    Desafio Classificador de nível de heroí

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-teste
+// Definir o nome e a quantidade de XP do heroí
+let nomeDoHeroi = "Homem de Ferro"
+let xp = 10000
+
+// Estrutura de decisão para determinar o nível do heroí
+let nivel
+
+if (xp < 1000) {
+    nivel = "Ferro"
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze"
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata"
+} else if (xp >= 6001 && xp <= 7000) {
+    nivel = "Ouro"
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina"
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente"
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal"
+} else {
+    nivel = "Radiante"
+}
+
+//Exibir a mensagem com o nível e nome do heroí
+console.log("O Heroí de nome " + nomeDoHeroi + " está no nível de " + nivel + "!")
+
